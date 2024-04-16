b16d4471d36f7c291c82d343c3bb3f8fe2825202
Mark Moxon <mark@moxon.net>
2024-04-16 16:26:00 +0100

    Fix incorrect file headers

--- 1-source-files/main-sources/elite-boot-disc.asm
+++ 1-source-files/main-sources/elite-boot-disc.asm
@@ -1,19 +1,17 @@
 \ ******************************************************************************
 \
-\ 6502 SECOND PROCESSOR ELITE BOOT FILE
+\ DISC ELITE BOOT FILE
 \
-\ 6502 Second Processor Elite was written by Ian Bell and David Braben and is
-\ copyright Acornsoft 1985
+\ Elite was written by Ian Bell and David Braben and is copyright Acornsoft 1984
 \
-\ The code on this site is identical to the source discs released on Ian Bell's
-\ personal website at http://www.elitehomepage.org/ (it's just been reformatted
-\ to be more readable)
+\ The code on this site has been reconstructed from a disassembly of the version
+\ released on Ian Bell's personal website at http://www.elitehomepage.org/
 \
 \ The commentary is copyright Mark Moxon, and any misunderstandings or mistakes
 \ in the documentation are entirely my fault
 \
 \ The terminology and notations used in this commentary are explained at
-\ https://www.bbcelite.com/about_site/terminology_used_in_this_commentary.html
+\ https://www.bbcelite.com/terminology
 \
 \ The deep dive articles referred to in this commentary can be found at
 \ https://www.bbcelite.com/deep_dives
